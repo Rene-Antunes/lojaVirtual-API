d1c2f5d1ebcdebc48c31899879c48faf6ba9e0fd
Renê Antunes Ladeira <93138911+Rene-Antunes@users.noreply.github.com>
2023-12-20 12:49:45 -0300

    feat: Criado classe PedidoFilter

---
+++ LojaVitual/src/main/java/com/lojavirtual/domain/filter/ProdutoFilter.java
@@ -0,0 +1,15 @@
+package com.lojavirtual.domain.filter;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoFilter {
+	
+	private String nomeProduto;
+	private String categoria;
+	private Boolean ativo;
+	
+	
+}
